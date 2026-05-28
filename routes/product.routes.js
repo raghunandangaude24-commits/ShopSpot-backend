e78423009ed038e647d84677617a79ec231cb902
raghunandangaude24-commits <raghunandangaude24@gmail.com>
2026-05-28 22:14:51 +0530

    updates

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const router = express.Router();
-const product = require("../controllers/product.controller");
 
-router.get("/", product.getProducts);
-router.post("/", product.addProduct);
+const router = express.Router();
+
+const productController =
+    require("../controllers/product.controller");
+
+/* ================= GET ALL PRODUCTS ================= */
+
+router.get("/", productController.getProducts);
+
+/* ================= GET SINGLE PRODUCT ================= */
+
+router.get("/:id", productController.getSingleProduct);
+
+/* ================= ADD PRODUCT ================= */
+
+router.post("/", productController.addProduct);
+
+/* ================= DELETE PRODUCT ================= */
+
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
