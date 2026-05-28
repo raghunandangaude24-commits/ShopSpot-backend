5a6d2b7c0d197e7e11fed96a7142b937cfc3403a
raghunandangaude24-commits <raghunandangaude24@gmail.com>
2026-05-28 22:33:45 +0530

    update

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,20 +5,52 @@ const router = express.Router();
 const productController =
     require("../controllers/product.controller");
 
+/* ================= AUTH MIDDLEWARE ================= */
+
+const {
+    protect,
+    admin
+} = require("../middleware/auth.middleware");
+
 /* ================= GET ALL PRODUCTS ================= */
 
-router.get("/", productController.getProducts);
+router.get(
+    "/",
+    productController.getProducts
+);
 
 /* ================= GET SINGLE PRODUCT ================= */
 
-router.get("/:id", productController.getSingleProduct);
+router.get(
+    "/:id",
+    productController.getSingleProduct
+);
 
 /* ================= ADD PRODUCT ================= */
 
-router.post("/", productController.addProduct);
+router.post(
+    "/",
+    protect,
+    admin,
+    productController.addProduct
+);
+
+/* ================= UPDATE PRODUCT ================= */
+
+router.put(
+    "/:id",
+    protect,
+    admin,
+    productController.updateProduct
+);
 
 /* ================= DELETE PRODUCT ================= */
 
-router.delete("/:id", productController.deleteProduct);
+router.delete(
+    "/:id",
+    protect,
+    admin,
+    productController.deleteProduct
+);
 
 module.exports = router;
