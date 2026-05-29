e11f961d3578c00bd28008990a04f0a1f4112c20
raghunandangaude24-commits <raghunandangaude24@gmail.com>
2026-05-29 23:24:05 +0530

    Update auth.middleware.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,16 +1,39 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    const token = req.header("Authorization");
 
-    if (!token) return res.status(401).json({ message: "No token" });
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader) {
+
+        return res.status(401).json({
+            message: "No token"
+        });
+    }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Remove "Bearer "
+        const token = authHeader.startsWith("Bearer ")
+            ? authHeader.slice(7)
+            : authHeader;
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
         req.user = decoded;
+
         next();
+
     } catch (err) {
-        res.status(401).json({ message: "Invalid token" });
+
+        console.log(err);
+
+        res.status(401).json({
+            message: "Invalid token"
+        });
     }
 };
 
