22f2d93ab6a1ed2cdedff3869f4355b9c3f41ce6
raghunandangaude24-commits <raghunandangaude24@gmail.com>
2026-05-30 23:04:33 +0530

    Update cart.routes.js

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+
 const auth = require("../middleware/auth.middleware");
 const cart = require("../controllers/cart.controller");
 
+// GET CART
 router.get("/", auth, cart.getCart);
+
+// ADD TO CART
 router.post("/add", auth, cart.addToCart);
+
+// UPDATE QUANTITY
+router.put("/update/:productId", auth, cart.updateQuantity);
+
+// REMOVE ITEM
+router.delete("/remove/:productId", auth, cart.removeFromCart);
 
 module.exports = router;
